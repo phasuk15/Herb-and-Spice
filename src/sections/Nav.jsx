85859334f92421654dfeca4e1148f53080cc8d83
phasuk15 <phasukdianas15@gmail.com>
2024-09-03 07:33:32 +0100

    half a nav bar lolol

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -4,8 +4,18 @@ import { Link as ScrollLink} from 'react-scroll';
 
 const Nav = () => {
   return (
-    <section className="fixed top-0 padding-x py-4 z-50 left-0 w-full bg-yellow-800">
-        Nav 
+    <section className="fixed top-0 padding-x py-4 z-50 left-0 w-full bg-yellow-800 border-boxShadow">
+        <nav className='flex justify-between items-center max-container z-50'>
+            <button>
+                bhjioacs
+            </button>
+            <ul className='flex-1 flex justify-end items-center gap-16 text-gray-300 max-md:hidden'>
+                <ScrollLink to="about" smooth={true} duration={500} className="hover:text-white cursor-pointer">About us</ScrollLink>
+                <ScrollLink to="projects" smooth={true} duration={500} className="hover:text-white cursor-pointer">Menu</ScrollLink>
+                <ScrollLink to="projects" smooth={true} duration={500} className="hover:text-white cursor-pointer">Bookings</ScrollLink>
+                <ScrollLink to="projects" smooth={true} duration={500} className="hover:text-white cursor-pointer">Find us</ScrollLink>
+            </ul>
+        </nav> 
     </section>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '5xl': ['60px', '70px'],
+      '6xl': ['72px', '82px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        fira: ['Fira Sans', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#ECEEFF",
+        "background-colour": "#001220",
+        'wave-blue': "#3b4374",
+        'blue-purple': "#645d92",
+        'light-purple': "#8e79b1",
+        'purple-pink': "#b996cf",
+        'light-pink': "#e5b4ed",
+        'deep-blue': "#150e40"
+      },
+      boxShadow: {
+        '3xl': '0 15px 40px rgba(0, 0, 0, 0.1)'
+      },
+      screens: {
+        "wide": "1440px"
+      },
+      animation: {
+        'bounce-once': 'bounce-once 1s ease',
+      }
+    },
+  },
+  plugins: [],
+}
+
