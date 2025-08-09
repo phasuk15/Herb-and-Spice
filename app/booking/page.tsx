b13c36979d053c3b1b3936e620d6f06cd602996e
Sye Phasuk <phasukdianas15@gmail.com>
2025-08-09 16:07:37 +0100

    Booking page done

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,85 +1,23 @@
 "use client";
 
 import BookingWidget from "@/components/BookingWidget";
-import { useState } from "react";
 
 const Booking = () => {
-  const [isCalendlyReady, setIsCalendlyReady] = useState(false);
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    date: "",
-    time: "",
-    guests: 1,
-  });
-
   return (
     <>
-      <section className="max-w-lg mx-auto py-24">
-        <h2 className="text-3xl font-bold mb-6 text-center">Book a Table</h2>
+      <section className="max-w-lg mx-auto py-24 px-4">
+        <h2 className="text-3xl font-bold py-8 text-center">Book a Table</h2>
+        <div className="pb-4">
+          <p className="mb-6 text-center text-gray-700">
+            If you have any special requests or dietary requirements, please mention them in the booking form’s notes section.
+          </p>
+
+          <p className="mb-8 text-center text-gray-700 italic">
+            Note: For group bookings of 8 or more, please contact us directly at <a href="tel:01788 537735" className="text-blue-600 underline">+1 234 567 890</a>.
+          </p>
+        </div>
+
         <BookingWidget />
-        {/* <form onSubmit={handleSubmit} className="space-y-4">
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            value={formData.name}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            value={formData.email}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Phone Number"
-            value={formData.phone}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <input
-            type="time"
-            name="time"
-            value={formData.time}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <input
-            type="number"
-            name="guests"
-            placeholder="Guests"
-            min={1}
-            value={formData.guests}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-          />
-          <button
-            type="submit"
-            disabled={!isCalendlyReady}
-            className={`w-full py-2 rounded text-white ${
-              isCalendlyReady
-                ? "bg-gray-800 hover:bg-gray-700"
-                : "bg-amber-500 cursor-not-allowed"
-            }`}
-          >
-            Reserve
-          </button>
-        </form> */}
       </section>
     </>
   );
